cfdd5fb9b0904adbf08f20bcafb48f0e76b2a29c
Rudizio320 <35193@DESKTOP-NELM0K3>
2022-06-13 19:42:15 +0100

    fusion

--- src/main/java/net/mcreator/teste/init/TesteModItems.java
+++ src/main/java/net/mcreator/teste/init/TesteModItems.java
@@ -13,11 +13,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.BlockItem;
 
+import net.mcreator.teste.item.FusionRecipeItem;
 import net.mcreator.teste.TesteMod;
 
 public class TesteModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, TesteMod.MODID);
 	public static final RegistryObject<Item> FUSION_ORE_TABLE = block(TesteModBlocks.FUSION_ORE_TABLE, CreativeModeTab.TAB_DECORATIONS);
+	public static final RegistryObject<Item> FUSION_RECIPE = REGISTRY.register("fusion_recipe", () -> new FusionRecipeItem());
 
 	private static RegistryObject<Item> block(RegistryObject<Block> block, CreativeModeTab tab) {
 		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
