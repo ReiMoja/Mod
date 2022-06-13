a45f8ae3523c3985337e954cc4b7e6cd0e86c219
ReiMoja <rodri@DESKTOP-0IN9MSF>
2022-06-13 19:57:08 +0100

    rfgfdg

--- src/main/java/net/mcreator/teste/FusionModMod.java
+++ src/main/java/net/mcreator/teste/FusionModMod.java
@@ -26,6 +26,10 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
+import net.mcreator.teste.init.FusionModModItems;
+import net.mcreator.teste.init.FusionModModBlocks;
+import net.mcreator.teste.init.FusionModModBlockEntities;
+
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
@@ -42,6 +46,10 @@ public class FusionModMod {
 	public FusionModMod() {
 
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		FusionModModBlocks.REGISTRY.register(bus);
+		FusionModModItems.REGISTRY.register(bus);
+
+		FusionModModBlockEntities.REGISTRY.register(bus);
 
 	}
 
